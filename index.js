f0b1e09f51281b6c01f176df9d7242d4fdd6ec61
vISHAL_TAWAR <vishalmtawar0205@gmail.com>
2022-05-31 18:37:52 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// let ary=["vishal","tawar","aakash","banjara"];
+// console.log(ary[2]);
+// for(let i=0;i<ary.length;i++)
+// {
+//     console.log(ary[i]); 
+// }
+// for(let a in ary)
+// {
+//     console.log(a);
+// }
+// for(let a of ary)
+// {
+//     console.log(a);
+// }
+// ary.forEach(function(element,index,array){
+//     console.log(element,index,array);
+//     //console.log(index);
+    
+// });
+// const sum=()=>{
+//     let a=5,b=20;
+//    return a+b;
+//     //return 'Sum of a and b = ${sum}';
+    
+// }
+// console.log(sum());
+
+// let ary=["vishal","tawar","aakash","banjara"];
+// console.log(ary.splice(0,1,"Tawar"));
+// console.log(ary);
+// let arr =[4,5,6,7,8];
+// let a = arr.map((currEle)=> currEle*2).filter((cE)=>cE>10)
+// console.log(a);
+//console.log(typeof(5-'9'));
+
+// let str="vishal, tawar, raj"
+// let s=str.substring(2)
+// console.log(s);
